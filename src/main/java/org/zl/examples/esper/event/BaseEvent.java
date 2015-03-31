47a2876da6fad3f36fbcdb19bf454ed1b25d3cf5
leozhang123 <leozhang1121@gmail.com>
2015-03-31 15:19:04 +0800

    init

---
+++ src/main/java/org/zl/examples/esper/event/BaseEvent.java
@@ -0,0 +1,42 @@
+/**
+ * Created on 2015年3月31日
+ */
+package org.zl.examples.esper.event;
+
+/**
+ * <br>
+ * change log:<br>
+ * 0.01 2015年3月31日 Created <br>
+ * @version 0.01
+ * @author Ray
+ */
+public class BaseEvent {
+
+	private int x;
+	private int y;
+	/**
+	 * @return the x
+	 */
+	public int getX() {
+		return x;
+	}
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+}
